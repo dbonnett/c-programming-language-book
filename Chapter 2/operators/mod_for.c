928dc6e7dfb657fcf99d62affd35ed81314349b4
Danny B <dbonnett1147@gmail.com>
2023-03-02 15:51:35 -0500

    continuation of Ch2

---
+++ Chapter 2/operators/mod_for.c
@@ -0,0 +1,10 @@
+/* modify the loop
+ * for (i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; i++)
+ * such that no &&s or ||s are used
+ */
+
+#include <stdio.h>
+
+
+for (i=0; i<lim-1; i++)
+/* nvm, they just want a series of if else statements */
---
+++ Chapter 2/types/const_array_func.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+}
+
+int stringLen(const char[]) {
+	for (int i = 0; i < LEN; i++) {
+		if 
